16780504b11235ba61c218618dccf53bfe2a3b1a
Freda Kemphrey <fredakemphrey17@gmail.com>
2025-02-19 11:31:48 +0000

    .

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,38 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage /> {/* Include the component inside the return statement */}
+    </div>
+  );
+}
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
+
+import UserProfile from './components/UserProfile';
+
+function App() {
+    return (
+        <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        </div>
+    );
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,5 +63,5 @@ function App() {
     </>
   )
 }
+export default App;
 
-export default App

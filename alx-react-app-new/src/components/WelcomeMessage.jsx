e230114f9bd42a57816918730d819df11dece28e
Freda Kemphrey <fredakemphrey17@gmail.com>
2025-02-21 13:50:34 +0000

    .

---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1 style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>Hello everyone, I am learning React at ALX!</h1>
+            <p style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>This is a simple JSX component.</p>
+            <p style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;

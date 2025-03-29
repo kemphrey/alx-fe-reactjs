e0954179fc2435590f3c08d762db1cf15893f42b
Freda Kemphrey <fredakemphrey17@gmail.com>
2025-03-29 12:23:40 +0000

    .

---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+export default function SearchBar() {
+    return (
+      <div className="p-4">
+        <input
+          type="text"
+          placeholder="Search GitHub username..."
+          className="border p-2 rounded w-full"
+        />
+      </div>
+    );
+  }
+  
